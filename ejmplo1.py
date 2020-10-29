a1c714aa2dab0b5d47b4db27d7a32bad51f4ebc0
Brandon Rodriguez <brandonrodriguez@MacBook-Air-de-Brandon-774.local>
2020-10-29 13:05:21 -0600

    imagenes

--- ejmplo1.py
+++ ejmplo1.py
@@ -7,6 +7,13 @@ print(imagen[0][0][0])
 imagen=cv2.resize(imagen,(256,256))
 
 cv2.imwrite("resiceimagen.jpg",imagen)
-cv2.imshow("image",imagen)
-cv2.waitKey(0)
+
+imagen=cv2.imread("imagen.jpg")
+imagen=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+print(imagen.shape)
+print(imagen[0][0])
+cv2.imwrite("grayimagen.jpg",imagen)
+
+#cv2.imshow("image",imagen)
+#cv2.waitKey(0)
 
